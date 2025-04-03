9976ec2163b0f8679c3b74afde2ac7c51a029eac
Ibrohim Fayzullayev <ibrohim_f01@mail.ru>
2025-04-03 12:52:36 +0500

    feat: update navigation to render DashboardScreen within AuthWrapper

--- src/infrastructure/navigation/index.tsx
+++ src/infrastructure/navigation/index.tsx
@@ -29,7 +29,7 @@ const Navigation = () => {
         path="/"
         element={
           <AuthWrapper>
-            <HomeScreen />
+            <DashboardScreen />
           </AuthWrapper>
         }
       />
